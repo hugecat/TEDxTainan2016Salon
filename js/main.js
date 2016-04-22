c89cdb537ed8dba18a4af29da5615455351326c6
hugecat <hugecat@Chieh-MBPR.local>
2016-04-22 17:33:58 +0800

    finish header logo

--- js/main.js
+++ js/main.js
@@ -79,5 +79,44 @@ jQuery(document).ready(function($){
 
 });
 
+	$(function () {
+
+			resize_tab();
+	});
+
+	$(window).resize(function () {
+			resize_tab();
+	}).resize();
+
+	/*function resize_tab() {
+
+			var viewportWidth = $(window).innerWidth();
+			var viewportHeight = $(window).innerHeight();
+
+			var width = $('#section2').width();
+			var height = $('#section2').height();
+
+
+			if ((viewportWidth / viewportHeight) > (width / height)) {
+
+					$('#section2').css({
+							'width': '100%',
+							'height': 'auto',
+							'margin-left': 0 - width / 2,
+							'margin-top': 0 - height / 2
+					});
+
+
+			} else {
+					$('#section2').css({
+							'width': 'auto',
+							'height': '100%',
+							'margin-left': 0 - width / 2,
+							'margin-top': 0 - height / 2
+					});
+			}
+	}*/
+
+
 
 });
